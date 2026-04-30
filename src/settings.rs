9f25e18a9c2414cda5052e4066c77e21630cc6ab
amigin <amigin@gmail.com>
2026-04-30 10:56:06 +0300

    Rename postgres_url to postgres_conn_string in SettingsModel

--- src/settings.rs
+++ src/settings.rs
@@ -3,13 +3,13 @@ use serde::*;
 
 #[derive(Debug, Serialize, Deserialize, Clone, my_settings_reader::SettingsModel)]
 pub struct SettingsModel {
-    pub postgres_url: String,
+    pub postgres_conn_string: String,
 }
 
 #[async_trait::async_trait]
 impl PostgresSettings for SettingsReader {
     async fn get_connection_string(&self) -> String {
         let read_access = self.settings.read().await;
-        read_access.postgres_url.clone()
+        read_access.postgres_conn_string.clone()
     }
 }
